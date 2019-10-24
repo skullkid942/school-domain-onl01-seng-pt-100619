608091b559c1e795f477a4baabf51850d7799a1f
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-24 18:04:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,10 @@ class School
   end 
   
   def add_student(name, grade)
-    
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
   end 
 end 
